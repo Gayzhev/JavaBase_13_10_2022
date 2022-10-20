d6557ec6d212f8b418b1248b847efb59019e2352
Igor <111637532+Gayzhev@users.noreply.github.com>
2022-10-20 21:18:08 +0300

    Update HomeWork2.java

--- src/main/java/com/hillel/gayzhevskiy/lessons/lesson2/HomeWork2.java
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson2/HomeWork2.java
@@ -6,6 +6,6 @@ public class HomeWork2 {
         double y = 30.731747128835654;
         char z = 176;
         System.out.println("coordinate of my home: " + x + z + ", " + y + z);
-
+        System.out.println("coordinate of my home: " + x + z + ", " + y + z);
     }
 }
