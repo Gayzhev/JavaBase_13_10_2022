d203a298fe829c7d12e42cd3bd6cddb204a16a45
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-20 20:19:27 +0300

    git init commit

---
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson1/Main.java
@@ -0,0 +1,15 @@
+package com.hillel.gayzhevskiy.lessons.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte age;
+        byte age2;
+        byte age3;
+        int someInt;
+        someInt = 2_000_000_000;
+
+
+        age = -128;
+        System.out.println(someInt);
+      }
+}
---
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson2/HomeWork2.java
@@ -0,0 +1,10 @@
+package com.hillel.gayzhevskiy.lessons.lesson2;
+
+public class HomeWork2 {
+    public static void main(String [] args)  {
+        double x = 46.40614997082829;
+        double y = 30.731747128835654;
+        char z = 176;
+        System.out.println("coordinate of my home: " + x + z + ", " + y + z);
+    }
+}
