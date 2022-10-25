b25dd83d15713ff66f81852b57e81d43664dd848
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-25 17:22:23 +0300

    homework4

--- src/main/java/com/hillel/gayzhevskiy/lessons/lesson4/Main.java
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson4/Main.java
@@ -8,9 +8,13 @@ System.out.println("Об'єм паралелепіпеда = " + volume);
 Порахувати сумарну довжину всіх сторін у змінній length та вивести її на екран;
  */
 public class Main {
-double sideX = 2.0;
-double sideY = 3.0;
-double sideZ = 5.0;
-double square = sideX * sideY * sideZ;
-
+    public static void main(String[] args) {
+        double sideX = 2.0;
+        double sideY = 3.0;
+        double sideZ = 5.0;
+        double volume = sideX * sideY * sideZ;
+        double length = 4 * (sideX + sideZ + sideY);
+        System.out.println("Сума сторiн паралелепіпеда = " + length);
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+    }
 }
