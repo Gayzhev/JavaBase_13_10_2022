da26e8cb2e2e3cf5a27d293ae084bd38df84c9a2
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-25 21:45:45 +0300

    homework5

--- src/main/java/com/hillel/gayzhevskiy/lessons/lesson4/Homework5.java
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson4/Homework5.java
@@ -14,7 +14,18 @@ package com.hillel.gayzhevskiy.lessons.lesson4;
 Вивести в консоль значення загальної атаки кожної армії; */
 public class Homework5 {
     public static void main(String[] args) {
-
-
+        int VoinLi = 13;
+        int LuchnikLi = 24;
+        int VershnikLi = 46;
+        int VoinMin = 9;
+        int LuchnikMin = 35;
+        int VershnikMin = 12;
+        int NumberLi = 860;
+        double ratioQuantity = 1.50;
+        int NumberMin = (int) (NumberLi * ratioQuantity);
+        int fullPowerLi = (VoinLi * LuchnikLi * VershnikLi) * NumberLi;
+        int fullPowerMin = (VoinMin * LuchnikMin * VershnikMin) * NumberMin;
+        System.out.println(fullPowerMin);
+        System.out.println(fullPowerLi);
     }
 }
