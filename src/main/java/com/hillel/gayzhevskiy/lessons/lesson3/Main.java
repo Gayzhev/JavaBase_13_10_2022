fcff968c3b5ed54a7190a1c2ad423f1af17d2329
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-25 21:45:35 +0300

    homework5

---
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson3/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.gayzhevskiy.lessons.lesson3;
+
+public class Main {
+    public static void main(String[]args) {
+    int a = 10;
+    int b = 3;
+System.out.println(a + b);
+System.out.println((double) a / b);
+System.out.println(a % b);
+
+
+    }
+}
