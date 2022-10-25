754c471de8a2a41fa145b94e489ff5225d9718b5
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-25 17:07:00 +0300

    homework4

---
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson4/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.gayzhevskiy.lessons.lesson4;
+/* Паралелепіпед:
+Створити у новому класі main-метод;
+Створити три змінні для зберігання сторін паралелепіпеда;
+Порахувати обсяг у змінній volume;
+Вивести на екран змінну volume у вигляді:
+System.out.println("Об'єм паралелепіпеда = " + volume);
+Порахувати сумарну довжину всіх сторін у змінній length та вивести її на екран;
+ */
+public class Main {
+double sideX = 2.0;
+double sideY = 3.0;
+double sideZ = 5.0;
+double square = sideX * sideY * sideZ;
+
+}
