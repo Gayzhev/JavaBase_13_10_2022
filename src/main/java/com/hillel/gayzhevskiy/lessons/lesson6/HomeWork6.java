0aac8d2c36a0e1c9ddbe9701c1af79c58ea108bb
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-10-30 18:54:16 +0200

    homework5

---
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson6/HomeWork6.java
@@ -0,0 +1,40 @@
+package com.hillel.gayzhevskiy.lessons.lesson6;
+/*    ввести ім'я першої команди
+    ввести кількість фрагів для 5-ти гравців першої команди (тобто має бути 5 змінних)
+    ввести ім'я другої команди
+    ввести кількість фрагів для 5-ти гравців другої команди (тобто має бути 5 змінних)
+    підрахувати середнє арифметичне балів кожної команди
+    порівняти результати команд та визначити переможця
+    вивести у консоль:
+
+"Перемогла команда" + teamName + "набрала" + result + "очків" */
+
+public class HomeWork6 {
+    public static void main(String[] args) {
+        String team1 = "WarMade";
+        int latHam = 4;
+        int chatHam = 9;
+        int flyRad = 2;
+        int boyRod = 3;
+        int gomMes = 4;
+        int averagePowerWarMade = (latHam + chatHam + flyRad + boyRod + gomMes) / 2;
+        String team2 = "MurDer";
+        int latBam = 4;
+        int chatFan = 4;
+        int flyObj = 6;
+        int boyTorn = 4;
+        int goHome = 2;
+        int averagePowerMurDer = (latBam + chatFan + flyObj + boyTorn + goHome) / 2;
+        if (averagePowerMurDer > averagePowerWarMade) {
+            System.out.println("Winning team - " + team1 + ". Score: " + averagePowerWarMade + " frags");
+            if (averagePowerMurDer < averagePowerWarMade) {
+                System.out.println("Winning team - " + team2 + ". Score: " + averagePowerMurDer + " frags");
+            }
+        } else {
+            System.out.println("Result - tie points. Score: " + averagePowerMurDer + " frags");
+        }
+    }
+}
+
+
+
