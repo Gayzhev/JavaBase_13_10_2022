8828df76b1cc282a0c6a2887a1a328772d539470
Gayzhevskiy Igor <gayzhev@gmail.com>
2022-11-12 16:28:48 +0200

    homework9

--- src/main/java/com/hillel/gayzhevskiy/lessons/lesson9/HomeWork9.java
+++ src/main/java/com/hillel/gayzhevskiy/lessons/lesson9/HomeWork9.java
@@ -22,15 +22,15 @@ public class HomeWork9 {
         for (int i = 0; i < arrayTeam2.length; i++) {
             arrayTeam2[i] = (int) (Math.random() * range) + minAge;
         }
-        System.out.println("Age of first team :" + Arrays.toString(arrayTeam1));
-        System.out.println("Age of second team :" + Arrays.toString(arrayTeam2));
+        System.out.println("Age of first  team players :" + Arrays.toString(arrayTeam1));
+        System.out.println("Age of second team players :" + Arrays.toString(arrayTeam2));
 
         int totalAge1 = 0;
         for (int i = 0; i < arrayTeam1.length; i++) {
             totalAge1 += arrayTeam1[i];
         }
         int averageAge1 = totalAge1 / arrayTeam1.length;
-        System.out.println("Average age of first team is  " + averageAge1);
+        System.out.println("Average age of first  team is  " + averageAge1);
 
         int totalAge2 = 0;
         for (int i = 0; i < arrayTeam2.length; i++) {
